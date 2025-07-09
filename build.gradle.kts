17fc53a8a8eaa8eb81fca7237608a47aadcbbd70
ahdeka <ahdeka01@gmail.com>
2025-07-09 17:51:15 +0900

    layout파일 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:3.1.1")
+	implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:3.2.1")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 
 	compileOnly("org.projectlombok:lombok")
